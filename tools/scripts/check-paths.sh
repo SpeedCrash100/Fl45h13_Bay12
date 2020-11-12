4169d471d19de826b090bafc162fbb153ee8377f
Vladimir Veisman <v.veisman@fl45h13.me>
2020-11-12 10:59:24 +0300

    Initial commit, Baystation 12 r1.1

---
+++ tools/scripts/check-paths.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+FAILED=0
+shopt -s globstar
+
+exactly() { # exactly N name search [mode] [filter]
+	count="$1"
+	name="$2"
+	search="$3"
+	mode="${4:--E}"
+	filter="${5:-**/*.dm}"
+
+	num="$(grep "$mode" "$search" $filter | wc -l || true)"
+
+	if [ $num -eq $count ]; then
+		echo "$num $name"
+	else
+		echo "$(tput setaf 9)$num $name (expecting exactly $count)$(tput sgr0)"
+		FAILED=1
+	fi
+}
+
+# With the potential exception of << if you increase any of these numbers you're probably doing it wrong
+exactly 0 "escapes" '\\\\(red|blue|green|black|b|i[^mc])'
+exactly 5 "Del()s" '\WDel\('
+exactly 2 "/atom text paths" '"/atom'
+exactly 2 "/area text paths" '"/area'
+exactly 2 "/datum text paths" '"/datum'
+exactly 2 "/mob text paths" '"/mob'
+exactly 12 "/obj text paths" '"/obj'
+exactly 8 "/turf text paths" '"/turf'
+exactly 150 "to_world uses" '\sto_world\('
+exactly 13 "to_world_log uses" '\sto_world_log\('
+exactly 1 "world<< uses" 'world<<|world[[:space:]]<<'
+exactly 47 "world.log<< uses" 'world.log<<|world.log[[:space:]]<<'
+exactly 744 "<< uses" '(?<!<)<<(?!<)' -P
+exactly 535 "incorrect indentations" '^( {4,})' -P
+exactly 33 "text2path uses" 'text2path'
+exactly 367 "update_icon() override" '/update_icon\((.*)\)'  -P
+exactly 2 "goto use" 'goto '
+exactly 860 "spawn uses" 'spawn\s*\(\s*(-\s*)?\d*\s*\)' -P
+exactly 513 "tag uses" '\stag = ' -P '**/*.dmm'
+exactly 299 "/global/ or /static/ vars defined" '/(global|static)/' -P
+# With the potential exception of << if you increase any of these numbers you're probably doing it wrong
+#
+#broken_files=0
+#while read -r file; do
+#	ftype="$(uchardet "$file")"
+#	case "$ftype" in
+#		ASCII)
+#			continue;;
+#		UTF-8)
+#			if diff -d "$file" <(<"$file" iconv -c -f utf8 -t iso8859-1 2>/dev/null | tr -d $'\x7F-\x9F' | iconv -c -f iso8859-1 -t utf8 2>/dev/null); then
+#				continue
+#			else
+#				echo "$file contains Unicode characters outside the ISO 8859-1 character set"
+#				(( broken_files = broken_files + 1 ))
+#			fi;;
+#		*)
+#			if diff -d "$file" <(<"$file" tr -d $'\x7F-\x9F' | iconv -c -f iso8859-1 -t utf8 2>/dev/null | iconv -c -f utf8 -t iso8859-1 2>/dev/null); then
+#				continue
+#			else
+#				echo "$file contains characters outside the ISO 8859-1 character set"
+#				(( broken_files = broken_files + 1 ))
+#			fi;;
+#	esac
+#done < <(find . -name '*.dm')
+#echo "$broken_files files with invalid characters"
+#if (( broken_files > 0 )); then
+#	FAILED=1
+#fi
+
+num=`find ./html/changelogs -not -name "*.yml" | wc -l`
+echo "$num non-yml files (expecting exactly 2)"
+[ $num -eq 2 ] || FAILED=1
+
+num=`find . -perm /111 -name "*.dm*" | wc -l`
+echo "$num executable *.dm? files (expecting exactly 0)"
+[ $num -eq 0 ] || FAILED=1
+
+exit $FAILED
